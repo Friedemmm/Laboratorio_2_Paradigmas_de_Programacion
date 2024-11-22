208ccabc2dd56ae2c7e810a5615674785c486f2a
Sofía Vergara Godoy <148838071+Friedemmm@users.noreply.github.com>
2024-11-21 22:48:46 -0300

    Update TDAboard_210821481_SofiaVergaraGodoy.pl

--- TDAboard_210821481_SofiaVergaraGodoy.pl
+++ TDAboard_210821481_SofiaVergaraGodoy.pl
@@ -37,16 +37,16 @@ myAppend([Cab|Cola], L2, [Cab|Resultado]) :-
 % Dominio: No recibe parámetros de entrada.
 % Estrategia: Creación sin backtracking.
 
-% Función crear el tablero.
+% Predicado crear el tablero.
 board(board(Board)) :-
     boardVacio(Board).
 
-% Función auxiliar para crear las 6 filas.
+% Predicado auxiliar para crear las 6 filas.
 boardVacio(Board) :-
     filaVacia(Fila),
     Board = [Fila, Fila, Fila, Fila, Fila, Fila].
     
-% Función auxiliar para crear una fila de 7 elementos.
+% Predicado auxiliar para crear una fila de 7 elementos.
     filaVacia([0, 0, 0, 0, 0, 0, 0]).
 
 %-------------------------OTROS-BOARD-------------------------%
@@ -55,7 +55,7 @@ boardVacio(Board) :-
 % Dominio: board(Board).
 % Estrategia: Verificación por recorrido de lista (fila superior; primera fila).
 
-% Función verificar si se puede jugar.
+% Predicado verificar si se puede jugar.
 can_play(Board) :-
     % Ver primera fila y buscar si hay espacio vacio.
     getFirst(Board, PrimeraFila),
@@ -67,13 +67,15 @@ can_play(Board) :-
 % Dominio: Board (board) X Column (int) X Piece (piece) X NewBoard (board).
 % Estrategia: *************
 
-% Función jugar la ficha.
+% Predicado jugar la ficha.
 play_Piece(board(Filas), Columna, Piece, board(NuevasFilas)) :-
     Columna >= 1, 
     Columna =< 7,
     ponerPiece(Filas, Columna, Piece, NuevasFilas).
 
-% Función auxiliar para poner la ficha en el board.
+
+↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓      CAMBIAR      ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+% Predicado auxiliar para poner la ficha en el board.
 ponerPiece(Filas, Columna, Piece, NuevasFilas) :-
     getColumna(Filas, ColAux, Columna),
     encontrarEspacio(Board, Columna, Filas),
@@ -82,8 +84,8 @@ ponerPiece(Filas, Columna, Piece, NuevasFilas) :-
     nuevaFila(FilaAnt, ColAux, Piece, FilaNueva),
     nuevaFila(Filas, Pos, FilaNueva, FilasNuevas).
     
-%........
-% Función auxiliar para entrar en la columna.
+
+% Predicado auxiliar para entrar en la columna.
 % Caso Base.
 get_column([], _, []).
 
@@ -92,8 +94,8 @@ getColumna([Fila|Filas], ColAux, [Elemento|Resto]) :-
     getElement(Fila, ColAux, Elemento),
     getColumna(Filas, ColAux, Resto).
     
-%........
-% Función auxiliar para encontrar en que fila de la columna especificada esta el primer 0.
+
+% Predicado auxiliar para encontrar en que fila de la columna especificada esta el primer 0.
 encontrarEspacio(Board, Columna, FilaPosicion) :-
     encontrarEspacioAux(Board, Columna, 1, FilaPosicion).
 
@@ -106,8 +108,8 @@ encontrarEspacioAux([_ | RestoFilas], Columna, FilaIndex, FilaPosicion) :-
     NextIndex is FilaIndex + 1,
     encontrarEspacioAux(RestoFilas, Columna, NextIndex, FilaPosicion).
     
-%........
-% Función auxiliar para sobreescribir la elemento.
+
+% Predicado auxiliar para sobreescribir la elemento.
 nuevaFila(Lista, Pos, ElementoNuevo, NuevaLista) :-
     myLength(Lista, Len),
     Pos =< Len,
@@ -120,4 +122,53 @@ nuevaFila(Lista, Pos, ElementoNuevo, NuevaLista) :-
 % Descripcion: Predicado que permite verificar ganador que conecta 4 fichas de forma vertical.
 % Dominio: board (board) X int (1 si gana jugador 1, 2 si gana jugador 2, 0 si no hay ganador vertical).
 % Estrategia: 
+
+% Predicado para verificar las conidiciones de win.
+check_vertical_win(board(Board), Winner) :-
+    Columnas = [1,2,3,4,5,6,7],
+    checkColumna(Board, Columnas, Winner).
+
+↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓      CAMBIAR      ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+% Predicado auxiliar para verificar cada columna.
+% Caso base.
+checkColumna(_, [], 0).
+
+% Caso recursivo; con ganador.
+checkColumna(Board, [Head|_], Winner) :-
+    getColumna(Board, Head, ColumnaRevisar),
+    checkConsecutivos(ColumnaRevisar, Winner),
+    Winner \= 0.
+    
+% Caso recursivo; sin ganador.
+checkColumna(Board, [Head|Cola], Winner) :-
+    getColumna(Board, Head, ColumnaRevisar),
+    checkConsecutivos(ColumnaRevisar, 0),
+    checkColumna(Board, Cola, Winner).
+
+
+% Predicado para ver si hay 4 consecutivos.
+% Caso base.
+checkConsecutivos([], 0).
+
+% Caso base; no hay tantas piezas puestas.
+checkConsecutivos(List, 0) :-
+    myLength(List, Len),
+    Len < 4.
+
+% Caso base; no hay ganador.
+checkConsecutivos([_ | Cola], Winner) :-
+    checkConsecutivos(Cola, Winner).
+    
+% Caso recursivo.
+checkConsecutivos([P1, P2, P3, P4 | _], Winner) :-
+    P1 \= 0,  % No considerar espacios vacíos
+    P1 = P2,
+    P2 = P3,
+    P3 = P4,
+    Winner = P1.  % El ganador es el número del jugador (1 o 2).
+
+
+
+
+
 
