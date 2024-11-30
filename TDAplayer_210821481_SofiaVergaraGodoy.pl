3838595a8b8cee858edbcced5c2337f1c5fad5d0
Sofía Vergara Godoy <148838071+Friedemmm@users.noreply.github.com>
2024-11-29 23:09:54 -0300

    Update TDAplayer_210821481_SofiaVergaraGodoy.pl

--- TDAplayer_210821481_SofiaVergaraGodoy.pl
+++ TDAplayer_210821481_SofiaVergaraGodoy.pl
@@ -1,16 +1,29 @@
 %---------------------SELECTORES-PLAYER-----------------------%
 
-getPlayer(ID, Name, Color, Wins, Losses, Draws, RemainingPieces) :-
-    player(ID, Name, Color, Wins, Losses, Draws, RemainingPieces, _).
+getID(Player, ID) :-
+    player(ID, _, _, _, _, _, _, Player).
+getName(Player, Name) :-
+    player(_, Name, _, _, _, _, _, Player).
+getColor(Player, Color) :-
+    player(_, _, Color, _, _, _, _, Player).
+getWin(Player, Win) :-
+    player(_, _, _, Win, _, _, _, Player).
+getLoss(Player, Loss) :-
+    player(_, _, _, _, Loss, _, _, Player).
+getDraw(Player, Draw) :-
+    player(_, _, _, _, _, Draw, _, Player).
+getPiece(Player, Piece) :- 
+    player(_, _, _, _, _, _, Piece, Player).
 
-%--------------------CONSTRUCTOR-PLAYER-----------------------%
+
+%-----------------RF-02-CONSTRUCTOR-PLAYER--------------------%
 
 % Descripcion: Predicado que permite crear un jugador.
 % Dominio: id (int) X name (string) X color (string) X wins (int) X losses (int) X draws (int) X remaining_pieces (int) X Player.
 % Meta Principal: player/8.
 % Meta Secundaria:...
 
-player(ID, Name, Color, Wins, Losses, Draws, RemainingPieces, player(ID, Name, Color, Wins, Losses, Draws, RemainingPieces)) :-
+player(ID, Name, Color, Wins, Losses, Draws, RemainingPieces, [ID, Name, Color, Wins, Losses, Draws, RemainingPieces]) :-
     % Verificaciones.
     integer(ID),
     ID > 0,
