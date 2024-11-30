b4d25104ddb950f20799e065f7b43ee44f54f5a7
Sofía Vergara Godoy <148838071+Friedemmm@users.noreply.github.com>
2024-11-30 00:56:44 -0300

    Add files via upload

---
+++ main_210821481_SOFIA_VERGARAGODOY.pl
@@ -0,0 +1,4 @@
+:- consult('TDAboard_210821481_SofiaVergaraGodoy.pl').
+:- consult('TDAgame_210821481_SofiaVergaraGodoy.pl').
+:- consult('TDApiece_210821481_SofiaVergaraGodoy.pl').
+:- consult('TDAplayer_210821481_SofiaVergaraGodoy.pl').
